0ef24ba7a03f0e439df82502f3555c1a829cb3ac
Daniel Primo <delineas@gmail.com>
2023-01-11 10:45:46 +0100

    step03 Components in app directory

--- app/about-extra/page.js
+++ app/about-extra/page.js
@@ -1,5 +1,5 @@
 function about() {
-  return <div>Hola, este es la típica página de Sobre mi</div>;
+  return <main>Hola, este es la típica página de Sobre mi pero en la carpeta `app`</main>;
 }
 
 export default about;
--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,17 @@
+import "../styles/globals.css";
+
+import Footer from "../components/footer";
+import Header from "../components/header";
+
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body>{children}</body>
+      <body>
+        <Header></Header>
+        {children}
+        <Footer></Footer>
+      </body>
     </html>
-  )
+  );
 }
