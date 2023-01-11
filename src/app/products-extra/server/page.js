10e806e214bcfc32779ef91deb5adda47e44167b
Daniel Primo <delineas@gmail.com>
2023-01-11 19:02:00 +0100

    step06 Server render no-store cache in app directory

---
+++ src/app/products-extra/server/page.js
@@ -0,0 +1,32 @@
+
+
+async function getProducts() {
+  const response = await fetch("https://dummyjson.com/products", {
+    cache: "no-store",
+  });
+  const data = await response.json();
+  return data.products;
+}
+
+async function ProductsExtraStaticIndex() {;
+
+  const products = await getProducts()
+
+  return (
+    <main>
+      <h1>Static products</h1>
+      <section>
+        {products.map((product) => {
+          return (
+            <aside key={product.id}>
+              <img src={product.thumbnail} />
+              <h3>{product.title}</h3>
+            </aside>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default ProductsExtraStaticIndex;
