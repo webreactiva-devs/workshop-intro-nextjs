4cc42b9a19814069202d037d3aebe1a7f3090af4
Daniel Primo <delineas@gmail.com>
2023-01-11 18:58:35 +0100

    step06 getServerSideProps

---
+++ src/pages/products/server/index.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function ProductsServerIndex({ products }) {
+  return (
+    <main>
+      <h1>Server products</h1>
+      <section>
+        {products.map((product) => {
+          return (
+            <aside key={product.id}>
+              <img src={product.thumbnail} />
+              <h3>{product.title}</h3>
+            </aside>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export async function getServerSideProps() {
+  const response = await fetch("https://dummyjson.com/products");
+  const data = await response.json();
+  const products = data.products;
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
+
+export default ProductsServerIndex;
