7e5731cd363f3e6cceba9d2af19b12803c4cc81b
Daniel Primo <delineas@gmail.com>
2023-01-11 10:29:29 +0100

    step02 Add MVP.css and replace index.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" color-mode="user">
       <Head />
       <body>
         <Main />
