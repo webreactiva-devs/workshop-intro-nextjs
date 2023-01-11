39747c8788e8b4ca2dbde3f69c478ef82a34ae22
Daniel Primo <delineas@gmail.com>
2023-01-11 12:08:31 +0100

    step04 CSR List products page app directory - use client

---
+++ src/app/products-extra/page.js
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+function ProductsExtraIndex() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <main>
+      <section>
+        {products.map((product) => {
+          return (
+            <aside key={product.id}>
+              <img src={product.thumbnail} />
+              <h3>{product.title}</h3>
+            </aside>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default ProductsExtraIndex;
