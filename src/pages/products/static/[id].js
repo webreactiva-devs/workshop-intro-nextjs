b76a6c30e49bb0be1c91f3e9942c1d559a7cec46
Daniel Primo <delineas@gmail.com>
2023-01-16 20:06:35 +0100

    step09 Add static id product

---
+++ src/pages/products/static/[id].js
@@ -0,0 +1,51 @@
+function ProductDetailStaticIndex({ product }) {
+  return (
+    <main>
+      <h1>{product.title}</h1>
+      <article>
+        <aside>
+          <p>
+            {product.brand} / {product.category}
+          </p>
+        </aside>
+      </article>
+      <section>
+        <aside>
+          <img alt={product.title} src={product.thumbnail} height="150" />
+          <h3>{product.title}</h3>
+          <p>{product.description}</p>
+          <header>
+            <h2>{product.price}€</h2>
+            <p>
+              <a href="#">
+                <em>Comprar ↗</em>
+              </a>
+            </p>
+          </header>
+        </aside>
+      </section>
+    </main>
+  );
+}
+
+export async function getStaticProps({params}) {
+  console.log(`https://dummyjson.com/products/${params.id}`);
+  const response = await fetch(`https://dummyjson.com/products/${params.id}`);
+  const data = await response.json();
+  const product = data;
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: false,
+  };
+}
+
+export default ProductDetailStaticIndex;
